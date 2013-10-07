0772f4a49353b790463230d2d974cfeec2b758ae
Michael Stewart <michael_stewart+gitdev@vt.edu>
2013-10-07 11:45:53 -0400

    beat model toggles fill

--- app/scripts/models/beat.js
+++ app/scripts/models/beat.js
@@ -1,21 +1,27 @@
-function Beat($rootScope) {
+function Beat($rootScope, beatId) {
   this.$rootScope = $rootScope;
   this.enabled = false;
-  this.opacity = 1;
+  this.fill = 'black';
+  this.id = beatId;
   this.decrement = function() {
     this.opacity = this.opacity - 0.1;
     console.log('decremented to: '+this.opacity);
   };
   this.toggle = function() {
-    this.opacity = (this.opacity + 1)%2;
-    console.log('toggled opacity');
+    if (this.fill == 'black') {
+      this.fill = 'none';
+    } else {
+      this.fill = 'black';
+    }
+    console.log('toggled fill');
   };
 }
 
 angular.module('sofaApp.models', [])
   .factory('Beat', function ($injector) {
-     return function(verb, args) { 
+     return function(id) { 
       return $injector.instantiate( Beat, { 
+        beatId: id
       }); 
     };
   });
