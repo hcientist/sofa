bb30a5e67bd331e2f5c96d88dc9a1a14402c2332
Michael Stewart <michael_stewart+gitdev@vt.edu>
2013-10-07 11:46:13 -0400

    get directive and behavior

--- app/scripts/directives/beat.js
+++ app/scripts/directives/beat.js
@@ -3,11 +3,38 @@ angular.module('sofaApp.directives',[]).directive('beat', function() {
     restrict: 'E',
     replace: true,
     scope: {
-      opacity: '@',
+      fill: '=',  // the fact that this is only a symbol 
+                  // (without a name after it as in toggledir (below)),
+                  // means that on the element that this directive is 
+                  // attached to, there should be an attribute by this name:
+                  // fill
+                  // the fact that this symbol is a = (and not a @ or &) 
+                  // means that in the scope of the element for this directive 
+                  // (not the elem defined in this template, but the one on 
+                  // which this directive is applied), the value passed into 
+                  // this attribute should exist
+      id: '@bid', 
       toggleDir: '&toggle'
     },
     link: function(scope, elem, attr, ctrl) {
+      var container = d3.select(elem[0]).append('svg')
+      .attr('width', 100)
+      .attr('height', 100);
+
+      var beat = container.insert('rect')
+        .attr('x',0)
+        .attr('y',0)
+        .attr('width', 50)
+        .attr('height', 50);
+      beat.attr('stroke', 'black')
+        .attr('fill', scope.fill);
+
+      scope.$watch('fill', function (newVal, oldVal) {
+          beat.attr('fill',newVal);
+      });
+
     },
     templateUrl: 'views/templates/beat.html'
   };
 });
+
